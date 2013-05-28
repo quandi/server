19e92b5fdcbf0f76772bec176a3ae44178fc0539
周挺川 <420664779@qq.com>
2013-05-28 19:50:14 +0800

    BGS

---
+++ BGS/src/com/quandi/bgs/util/FileUtil.java
@@ -0,0 +1,21 @@
+package com.quandi.bgs.util;
+
+import com.quandi.bgs.dao.LoginDAO;
+
+public class FileUtil
+{
+    LoginDAO loginDAO;
+    
+    /** 
+     * <一句话功能简述>
+     * <功能详细描述>
+     * @param args
+     * @see [类、类#方法、类#成员]
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
